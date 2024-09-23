d06ae77714bc9f01021a729d5d931bc984d03243
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-09-23 12:11:21 +0000

    test and run

--- Chapter 1/01_03b.sql
+++ Chapter 1/01_03b.sql
@@ -1,1 +1,19 @@
 -- Insert new grape flavor product into table
+INSERT INTO Product (
+  ProductID,
+  ProductCode,
+  ProductName,
+  Size,
+  Variety,
+  Price,
+  Status
+)
+VALUES (
+  17,
+  'MWPRA20',
+  'Mineral Water',
+  20,
+  'Grape',
+  '1.79',
+  'ACTIVE'
+)
